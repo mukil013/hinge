72f7615f46dbb5464751654e7dc08459de39a4f9
mukil <mukilmukil1947@gmail.com>
2023-09-24 16:06:11 +0530

    color change

--- lib/pages/Login page.dart
+++ lib/pages/Login page.dart
@@ -5,45 +5,63 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title:const Text(
-          'Login Page',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.blue,
-      ),
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              // Username field
-              TextFormField(
-                decoration:const InputDecoration(
-                  labelText: 'Username',
-                  border: OutlineInputBorder(),
+      body: SafeArea(
+        child: Container(
+          color: Color(0xff7D4F50),
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 25),
+            child: ListView(
+              children: [
+                SizedBox(height: 35),
+                Center(
+                  child: Text('Hinge',
+                    style: TextStyle(
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                        color: Color(0xffD1BE9C)
+                    ),),
                 ),
-              ),
-              const SizedBox(height: 20),
-              // Password field
-              TextFormField(
-                obscureText: true,
-                decoration:const InputDecoration(
-                  labelText: 'Password',
-                  border: OutlineInputBorder(),
+                SizedBox(height: 125),
+                TextField(
+                  decoration: InputDecoration(
+                    filled: true,
+                    fillColor: Colors.white70,
+                    border: UnderlineInputBorder(),
+                    labelText: 'Username'
+                  )
                 ),
-              ),
-              const SizedBox(height: 20),
-              // Login button
-              ElevatedButton(
-                onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) =>const home())),
-                child: Text('Login'),
-              ),
-            ],
+                SizedBox(height: 40),
+                TextField(
+                  decoration: InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white70,
+                      border: UnderlineInputBorder(),
+                      labelText: 'Password'
+                  ),
+                ),
+                SizedBox(height: 80),
+                Expanded(
+                    child: SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                          onPressed: () => Navigator.push(context, MaterialPageRoute(builder: (context) => home())),
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xffD1BE9C)
+                          ),
+                          child: const Text('Login',
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Color(0xff7D4F50)
+                          )
+                          )
+                      ),
+                    )
+                )
+              ],
+            ),
           ),
         ),
-      ),
+      )
     );
   }
 }
--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,11 +1,14 @@
 
 
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:hinge1/components/cards.dart';
 import 'package:hinge1/components/input.dart';
 import 'package:go_router/go_router.dart';
 import 'package:hinge1/draweroptions/feedback.dart';
 import 'package:hinge1/draweroptions/profile.dart';
+import 'package:hinge1/pages/Login%20page.dart';
 import 'package:hinge1/pages/market.dart';
 import '';
 import '../draweroptions/fav.dart';
@@ -18,139 +21,140 @@ class home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      extendBodyBehindAppBar: true,
       appBar: AppBar(
-        backgroundColor: Colors.blue,
+        backgroundColor: Color(0xff7D4F50),
         elevation: 0,
         title: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
-            const TextButton(onPressed: null, child: Text('Product',
-              style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18
-              ),)),
-            TextButton(onPressed: () {
-              Navigator.push(context, MaterialPageRoute(builder: (context) => market()));
-            }, child: const Text('Manufacturers',style: TextStyle(
-                color: Colors.white,
-                fontSize: 18
-            ))),
+            Text('Hinge'),
+            IconButton(onPressed: null, icon: Icon(Icons.search,color: Colors.white))
           ],
         ),
       ),
       drawer: Drawer(
+        backgroundColor: Color(0xff7D4F50),
           child: SafeArea(
-            child: ListView(
-              children:  [
-                const SizedBox(height: 125),
-                ListTile(
-                  leading:const Icon(Icons.person),
-                  title:const Text('Profile'),
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(builder: (context) => EditProfileUI()));
-                  },
-                ),
-                ListTile(
-                  leading:const Icon(Icons.favorite),
-                  title:const Text('Favorite'),
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => fav())),
-                ),
-                ListTile(
-                  leading: Icon(Icons.bookmark_border),
-                  title: Text('Order'),
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => order())),
-                ),
-                ListTile(
-                  leading: Icon(Icons.reviews),
-                  title: Text('Review'),
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => review())),
-                ),
-                ListTile(
-                  leading: Icon(Icons.history),
-                  title: Text('History'),
-                ),
+            child: Container(
+              child: ListView(
+                children:  [
+                  const SizedBox(height: 125),
+                  ListTile(
+                    leading:const Icon(Icons.person),
+                    title:const Text('Profile'),
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => EditProfileUI()));
+                    },
+                  ),
+                  ListTile(
+                    leading:const Icon(Icons.favorite),
+                    title:const Text('Favorite'),
+                    onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => fav())),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.bookmark_border),
+                    title: Text('Order'),
+                    onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => order())),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.reviews),
+                    title: Text('Review'),
+                    onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => review())),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.history),
+                    title: Text('History'),
+                  ),
 
-                ListTile(
-                  leading: Icon(Icons.settings),
-                  title: Text('Settings'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.help),
-                  title: Text('Help'),
-                ),
-                ListTile(
-                  leading: Icon(Icons.feedback),
-                  title: Text('Feedback'),
-                  onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => feedback())),
-                ),
-                ListTile(
-                  leading: Icon(Icons.exit_to_app),
-                  title: Text('Sign Out'),
-                ),
-              ],
+                  ListTile(
+                    leading: Icon(Icons.settings),
+                    title: Text('Settings'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.help),
+                    title: Text('Help'),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.feedback),
+                    title: Text('Feedback'),
+                    onTap: () => Navigator.push(context, MaterialPageRoute(builder: (context) => feedback())),
+                  ),
+                  ListTile(
+                    leading: Icon(Icons.exit_to_app),
+                    title: Text('Sign Out'),
+                    onTap:
+                    () => Navigator.push(context, MaterialPageRoute(builder: (context) => LoginPage()))
+                    ,
+                  ),
+                ],
+              ),
             ),
       ),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home , color: Colors.black54),label: 'home'),
-          BottomNavigationBarItem(icon: Icon(Icons.message, color: Colors.black54),label: 'message'),
-          BottomNavigationBarItem(icon: Icon(Icons.shopping_cart, color: Colors.black54),label: 'cart'),
-        ],
-      ),
-      body: ListView(
-        children: const [
-            SizedBox(height: 10),
-            input(),
-            SizedBox(height: 25),
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 15),
-                child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text('Top Categories',style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold
-                  ),),
-                  Icon(Icons.chevron_right)
-                ],
+            backgroundColor: Color(0xff7D4F50),
+            items: [
+              BottomNavigationBarItem(icon: Icon(Icons.home , color: Colors.black54),label: 'home'),
+              BottomNavigationBarItem(icon: Icon(Icons.message, color: Colors.black54),label: 'message'),
+              BottomNavigationBarItem(icon: Icon(Icons.shopping_cart, color: Colors.black54),label: 'cart'),
+            ],
+          ),
+      body: Container(
+        color: Color(0xffD1BE9C),
+        child: ListView(
+          children: const [
+              SizedBox(height: 25),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                  child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('Features',style: TextStyle(
+                      fontSize: 18,
+                      color: Color(0xff7D4F50)
+                    ),),
+                    Icon(Icons.chevron_right, color: Color(0xff7D4F50))
+                  ],
+                ),
               ),
-            ),
-            SizedBox(height: 10),
-            cards(),
-            SizedBox(height: 25),
+              SizedBox(height: 10),
+              cards(),
+              SizedBox(height: 25),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text('New Arrival',style: TextStyle(
+                        fontSize: 18,
+                        color: Color(0xff7D4F50)
+                    ),),
+                    Icon(Icons.chevron_right, color: Color(0xff7D4F50))
+                  ],
+                ),
+              ),
+              SizedBox(height: 10),
+              cards(),
+              SizedBox(height: 25),
             Padding(
               padding: EdgeInsets.symmetric(horizontal: 15),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('New Arrival',style: TextStyle(
+                  Text('Top Rated',style: TextStyle(
                       fontSize: 18,
-                      fontWeight: FontWeight.bold
+                      color: Color(0xff7D4F50)
                   ),),
-                  Icon(Icons.chevron_right)
+                  Icon(Icons.chevron_right ,color: Color(0xff7D4F50))
                 ],
               ),
             ),
             SizedBox(height: 10),
             cards(),
-            SizedBox(height: 25),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text('Explore',style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold
-                ),),
-                Icon(Icons.chevron_right)
-              ],
-            ),
-          ),
-          SizedBox(height: 10),
-          cards()
-          ],
+            SizedBox(height: 10)
+            ],
+        ),
       ),
     );
   }
